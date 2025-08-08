fe76836c33dd31ac9fd2bfa0ef03b25805926e9f
Saifeldin tamer Mohamed Elsayes <136191198+Saifeldinsais@users.noreply.github.com>
2025-08-08 05:13:26 +0300

    fixed login

--- Final Project/controllers/user.controller.js
+++ Final Project/controllers/user.controller.js
@@ -51,8 +51,6 @@ const signup = async (req, res) => {
       return res.status(400).json({ status: "fail", message: "Username already exists" });
     }
 
-
-    password = await bcrypt.hash(password, 10);
     const user = await User.create({ name, email, username, password, photo });
 
     // jwt
